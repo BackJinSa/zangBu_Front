9ae559e3c978ccf2610eb2b6c28cded68d1df53b
LeeHaeIn <98319061+saranghein@users.noreply.github.com>
2025-08-01 11:07:17 +0900

    feat: color system 설계 (#10)
    ## 🔥 이슈
    
    #6
    
    ## 📜 개요
    
    - color system 만들었습니다.
    - 예시로는 http://localhost:5173/color-guide 로 들어가면 볼 수 있어요 
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,32 @@ export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        // Brand Colors
+        brand: {
+          1: '#1C4025',
+          2: '#285934',
+          3: '#68A63C',
+          4: '#68A63C',
+          5: '#F2F2F2',
+        },
+        // Status Colors
+        status: {
+          1: '#0D8CE7',
+          2: '#DC3545',
+        },
+        // Text Colors
+        text: {
+          1: '#6B7280',
+          2: '#555555',
+          3: '#FFFFFF',
+        },
+        // Background Colors
+        bg: {
+          1: '#F2F2F2',
+          2: '#FFFFFF',
+        },
+      },
       screens: {
         // 커스텀 breakpoint 설정
         app: { max: '1023px' }, // 1024px 미만 (모바일/태블릿)
