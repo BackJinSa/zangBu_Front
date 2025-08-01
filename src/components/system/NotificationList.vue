0c9340852aa74959e0e77d185e74b99753b05cc4
KyungMin <85470797+kgm7642@users.noreply.github.com>
2025-08-01 17:50:14 +0900

    Feat/#1 (#20)
    ## 🔥 이슈
    
    - #1
    
    ## 📜 개요
    
    notification 컴포넌트 설계, 일부 기능 구현, 더미데이터 추가
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

---
+++ src/components/system/NotificationList.vue
@@ -0,0 +1,41 @@
+<!-- /src/components/system/NotificationList.vue -->
+<template>
+  <div class="notification-list">
+    <template v-if="!loading && notifications.length">
+      <NotificationCard
+        v-for="notification in notifications"
+        :key="notification.id"
+        :notification="notification"
+        @action="$emit('notification-action', $event)"
+      />
+    </template>
+    <NotificationEmpty v-else-if="!loading && !notifications.length" />
+    <div v-else class="loading">불러오는 중...</div>
+  </div>
+</template>
+
+<script setup>
+import NotificationCard from './NotificationCard.vue'
+import NotificationEmpty from './NotificationEmpty.vue'
+
+defineProps({
+  notifications: Array,
+  loading: Boolean,
+})
+</script>
+
+<style scoped>
+.notification-list {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-top: 20px;
+}
+
+.loading {
+  text-align: center;
+  color: #6c757d;
+  font-size: 14px;
+  margin-top: 40px;
+}
+</style>
