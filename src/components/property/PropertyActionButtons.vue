9f493480faa669eec1315464a233232cf0a4fcbf
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-08-01 09:15:14 +0900

    feat: 컴포넌트 설계 및 기본 axios 설계 (#8)
    ## 🔥 이슈
    - #7 
    <!--- -# 이슈번호 적으면 됩니다. -->
    
    ## 📜 개요
    컴포넌트 설계 및 axios 설계 했습니다 기본 BASE URL도 설정했으니 건드리지마십쇼.
    <!---- 변경 사항 및 관련 이슈에 대해 간단하게 작성하면됩니다. 
           어떻게보다 무엇을 왜 수정했는지 설명해주세요. -->
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [x] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

---
+++ src/components/property/PropertyActionButtons.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { usePropertyStore } from '@/stores/property/property';
+
+const propertyStore = usePropertyStore();
+</script>
+
+<template>
+  <div class="property-action-buttons">
+    <!-- 매물 등록/취소 버튼들이 들어갈 자리 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PropertyActionButtons',
+  data() {
+    return {
+      // 컴포넌트 데이터
+    }
+  },
+  methods: {
+    // 컴포넌트 메서드
+  },
+}
+</script>
+
+<style scoped>
+.property-action-buttons {
+  /* 스타일 */
+}
+</style>
