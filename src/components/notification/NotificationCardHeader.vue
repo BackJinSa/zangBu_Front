9900555f026d011d6d5ce90353dd28e021701634
KyungMin <85470797+kgm7642@users.noreply.github.com>
2025-08-04 10:47:53 +0900

    Feat/#1 (#26)
    ## 🔥 이슈
    
    - #1 
    
    ## 📜 개요
    
    패키지 system -> notification, payment 분리
    알림 컴포넌트 일부 수정
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [ ] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

---
+++ src/components/notification/NotificationCardHeader.vue
@@ -0,0 +1,11 @@
+<!-- NotificationCardHeader.vue -->
+<template>
+  <div>
+    <h2 class="text-base font-semibold text-gray-900">{{ title }}</h2>
+    <p class="mt-1 text-sm text-gray-700">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({ title: String, message: String })
+</script>
