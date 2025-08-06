6222e1f0cd38570e0977bfbcdca0a323c29be0df
suyeonan <88480976+suyeonan@users.noreply.github.com>
2025-08-06 13:12:40 +0900

    feat: 채팅방 나가기 (#68)
    ## 🔥 이슈
    
    #65
    
    ## 📜 개요
    
    채팅방 나가기 추가
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- src/pages/chat/ChatRoomView.vue
+++ src/pages/chat/ChatRoomView.vue
@@ -26,9 +26,9 @@
         </div>
 
         <!-- 우측 영역 (판매자, 구매자에 따라 다르게) -->
-        <div>
-          <!--<div v-if="isSeller"> -->
-          <div class="flex flex-col items-center text-sm">
+        <div class="flex items-center gap-5">
+          <!-- 판매자인 경우 거래 활성화 토글 -->
+          <div v-if="isSeller" class="flex flex-col items-center text-sm">
             <span class="mb-1">거래 활성화</span>
             <label class="relative inline-flex items-center cursor-pointer">
               <input type="checkbox" v-model="isActive" class="sr-only peer" />
@@ -40,19 +40,37 @@
               ></div>
             </label>
           </div>
+
+          <!-- 구매자인 경우 거래 상태 -->
+          <span
+            v-else
+            class="text-xs font-medium px-2 py-1 rounded"
+            :class="{
+              'bg-blue-100 text-blue-600': status === '진행',
+              'bg-yellow-100 text-yellow-600': status === '중',
+              'bg-gray-100 text-gray-500': status === '완료',
+            }"
+          >
+            거래 {{ status }}
+          </span>
+
+          <!-- 나가기 버튼 -->
+          <Button variant="button9" class="px-2 py-1 text-xs h-auto" @click="showModal = true"
+            >나가기</Button
+          >
         </div>
-        <!-- <div v-else>
-        <span
-          class="text-xs font-medium px-2 py-1 rounded"
-          :class="{
-            'bg-blue-100 text-blue-600': status === '진행',
-            'bg-yellow-100 text-yellow-600': status === '중',
-            'bg-gray-100 text-gray-500': status === '완료',
-          }"
-        >
-          거래 {{ status }}
-        </span>
-      </div> -->
+
+        <!-- 팝업 -->
+        <PopupModal
+          :is-visible="showModal"
+          title="정말 채팅방을 나가시겠습니까?"
+          message="이 작업은 되돌릴 수 없습니다."
+          cancel-text="취소"
+          confirm-text="확인"
+          @cancel="handleCancel"
+          @confirm="handleConfirm"
+          @close="showModal = false"
+        />
       </header>
 
       <!-- 채팅 내용 -->
@@ -111,6 +129,10 @@ import { useChatStore } from '@/stores/chat/chat'
 import { useStomp } from '@/utils/useStomp'
 import { useAuthStore } from '@/stores/auth/auth'
 
+import Button from '@/components/common/Button.vue'
+import PopupModal from '@/components/common/PopupModal.vue'
+
+
 const route = useRoute()
 const router = useRouter()
 const chatStore = useChatStore()
@@ -119,6 +141,27 @@ const { markAsRead } = useChatStore()
 const { connect, subscribeRoom, unsubscribeRoom, disconnect } = useStomp()
 const roomId = route.params.roomId // URL 파라미터로부터 채팅방 ID 가져오기
 const myUserId = authStore.userId
+
+//나가기 모달
+const showModal = ref(false)
+
+const handleCancel = () => {
+  //취소 클릭 시
+  console.log('취소됨')
+  showModal.value = false
+}
+
+const handleConfirm = async () => {
+  //나가기 클릭 시
+  console.log('확인됨')
+  try {
+    await chatStore.leaveChatRoom(roomId) // 채팅방 나가기 API 호출
+    showModal.value = false
+    router.push('/chat/list') // 채팅방 목록 페이지로 이동
+  } catch (err) {
+    console.error('채팅방 나가기 실패:', err)
+  }
+}
 
 onMounted(() => {
   // STOMP 연결
