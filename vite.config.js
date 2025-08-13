7fd906c2e732ad1823cd7c24ab14999fbca99dca
saranghein <saranghein@gmail.com>
2025-08-13 11:29:10 +0900

    test: 테스트를 위한 로그인 수정
    (#92)

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,14 @@ export default defineConfig({
   define: {
     global: 'globalThis', // Node.js의 global을 브라우저 globalThis로 매핑
   },
+  // 프록시 설정 - 개발 환경에서 /api 요청을 백엔드로 전달
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
