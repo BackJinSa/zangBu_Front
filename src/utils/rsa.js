6b0bed2a7965992eba9043238a3fb156f7bf6169
M6F\student <sojsnake@gmail.com>
2025-08-19 14:03:53 +0900

    feat: auth api 연동

--- src/utils/rsa.js
+++ src/utils/rsa.js
@@ -2,7 +2,7 @@ import JSEncrypt from 'jsencrypt'
 
 // 공개키는 백엔드에서 내려주는 걸 세션에 저장해두거나
 // 환경변수에 넣어둘 수도 있음
-const PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAglHMRpu5fbFGmjpOvE1dC5tq5beBNwLq9MwcU6majRhxyhgtbVSkz8QL7VIU/I2pMEldc/n0PQ+w7oMXpuFNVvk1odJz7BeBursGjKDT9ayRy56+EFlfQBLxbp4dSeyjZTmTzlV+4mce5LcT+S+Xc/rNqA6l8gQK76tcnbA+csTJxq3G5Vp9cwkZ7mqSAV7Cf1bnY3zLB+OImA8wKV5JqD2cY37v+7wk9V7/q9BQ4zVtek9/uvcZalChY7GXrX71KXjhg1UJ5QR0NaV3m+8Evcgfr4kRAqKpceH7Co7JneK0GHuz/STvgfrArKzvtLO2qZM1Iup3HOvwSPvSWlQ7gwIDAQAB"
+const PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjw0Mf/scsIw1AuClj21WbtMbemUTtxLrApjw1C+iV+KEA7n3olgLUnWl0PRwabKfD/GrLo8qDlhUyN4YYnQdFcQvolOu+yTp+mm8gH7go7m1vSYo5ugIpto31o+MVCvZ1X4mrfk8JEea/YaxQRFEcGde5JVFI6Jhz9chdlrjqnGDV3PvoQfmOayzw3BtB6jo3RO1xeNlzEWwIhrs+SQexmOuJSL53WkQ1fm8cEwTmF93bOXZ05rJouYzRxcsWk21Zk9vBlKXVA0EA+d0wmSJiBwC2N2/mm9g2NOrlrjzp1fnC6VP0jE+I9ExNaqTbfR0HZt+YghehDD4yL13fabLVQIDAQAB"
 
 export function encryptRsa(text) {
   const encryptor = new JSEncrypt()
